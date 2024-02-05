06b86a8dbf3f5c4fd6b707307a6a41a70f31a58f
Sepehr Samavati <carer1300@gmail.com>
2024-02-05 22:44:37 +0330

    improve stemmer/tokenizer

--- src/apps/documentProcess/modules/base.ts
+++ src/apps/documentProcess/modules/base.ts
@@ -4,7 +4,7 @@ export const removeWikiReferences = (text: string) => text.replaceAll(/.\[[0-9]+
 
 export const halfSpaceToFullSpace = (text: string) => text.replaceAll(/\u200c/g, ' ');
 
-const ignoreList = ["اگر", "اما", "پس", "تا", "چون", "چه", "خواه", "زیرا", "که", "لیکن", "نه", "نیز", "و", "ولی", "هم", "یا", "همچنین", 'تاکنون'];
+const ignoreList = ["اگر", "اما", "پس", "تا", "چون", "چه", "خواه", "زیرا", "که", "لیکن", "نه", "نیز", "و", "ولی", "هم", "یا", "همچنین", 'تاکنون', 'است'];
 export const removeIgnoredWords = (words: string[]) => words.filter(word => !ignoreList.includes(word));
 
 export const removeDiacritics = (text: string) => text.replaceAll(/(ّ|َ|ِ|ُ|ً|ٍ|ٌ|ْ|ء)/g, '').replaceAll(/(إ|أ)/g, 'ا').replaceAll(/(ي|ئ)/g, 'ی').replaceAll(/ؤ/g, 'و').replaceAll(/ك/g, 'ک').replaceAll(/(ۀ|ة)/g, 'ه');
