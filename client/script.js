a16e5dff0842db29353061c340e6866707a1f3b7
Sepehr Samavati <carer1300@gmail.com>
2024-02-11 16:49:06 +0330

    update client

--- client/script.js
+++ client/script.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const elements = {
         body: document.querySelector('body'),
         search: document.querySelector('#search'),
+        /** @type {HTMLDivElement} */
+        // @ts-ignore
         result: document.querySelector('#result'),
         /** @type {HTMLDivElement} */
         // @ts-ignore
@@ -15,6 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
         /** @type {HTMLInputElement} */
         // @ts-ignore
         searchInput: document.querySelector('#search input'),
+        /** @type {HTMLButtonElement} */
+        // @ts-ignore
+        submitButton: document.querySelector('#submit-query')
     };
 
     const fetchSuggestions = () => {
@@ -27,6 +32,83 @@ document.addEventListener('DOMContentLoaded', () => {
                     showSuggestions(value, res);
                 }
             });
+    };
+
+    const fetchResult = () => new Promise(resolve => {
+        const value = elements.searchInput.value;
+        elements.suggestion.innerHTML = '';
+        fetch(`/query`, {
+            method: "POST",
+            body: JSON.stringify({
+                queryString: value,
+                doPageRank: true
+            }),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }).then(r => r.json())
+            .then(res => {
+                if (res.ok) {
+                    showResult(value.split(' '), res);
+                }
+            }).finally(() => resolve(null));
+    });
+
+    /**
+     * 
+     * @param {string[]} queryWords
+     * @param {{
+     *  ok: boolean;
+     *  retrieveTime: number;
+     *  totalCount: number;
+     *  result: { content: string; tfIdfScore: number; pageRank: number; score: number; title: string; url: string; }[];
+     * }} result 
+     */
+    const showResult = (queryWords, result) => {
+        elements.result.style.display = 'unset';
+        elements.result.innerHTML = '';
+
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('wrapper');
+
+        const resultInfo = document.createElement('section');
+        resultInfo.classList.add('result-info');
+        resultInfo.innerHTML = `<b>${result.totalCount}</b> سند در <b>${result.retrieveTime}</b> میلی ثانیه بازیابی شد.`;
+
+        const resultList = document.createElement('div');
+        resultInfo.classList.add('result-list');
+
+        result.result.forEach(item => {
+            const resultItem = document.createElement('div');
+            resultItem.classList.add('result-item');
+
+            const title = document.createElement('div');
+            title.classList.add('title');
+            title.innerText = item.title;
+
+            const link = document.createElement('a');
+            link.classList.add('link');
+            link.href = item.url;
+            link.innerText = item.url;
+
+            const body = document.createElement('div');
+            body.classList.add('body');
+            body.innerText = item.content;
+
+            const info = document.createElement('div');
+            info.classList.add('info');
+            info.innerHTML = `<span>Score: ${item.score}</span>` + `<span>Page rank: ${item.pageRank}</span>` + `<span>TF-IDF Score: ${item.tfIdfScore}</span>`;
+
+            [title, link, body, info].forEach(el => resultItem.append(el));
+
+            resultList.append(resultItem);
+        });
+
+
+        wrapper.append(resultInfo);
+        wrapper.append(resultList);
+
+        elements.result.append(wrapper);
     };
 
     /**
@@ -54,5 +136,11 @@ document.addEventListener('DOMContentLoaded', () => {
             window.clearTimeout(inputTimer);
             fetchSuggestions();
         }, inputDelay);
+    });
+
+    elements.submitButton.addEventListener('click', async event => {
+        elements.submitButton.disabled = true;
+        await fetchResult();
+        elements.submitButton.disabled = false;
     });
 });
